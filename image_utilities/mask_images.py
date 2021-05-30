d62e10af70aa69bbc3a337bd8c1d2f476403adc6
ldemaz <lestes@clarku.edu>
2021-05-29 22:30:46 -0400

    Revised mask_images.py and added utils.

--- image_utilities/mask_images.py
+++ image_utilities/mask_images.py
@@ -1,36 +1,56 @@
-# Read in image stack, create and apply common mask band, and write out to
-# geotiff
 import os
-import numpy as np
 import rioxarray as rxr
-import glob
 import re
 
-# Set-up directories
-ROOT = os.path.abspath(os.getcwd())
-data_path = os.path.join(ROOT, "external/data/volumetric/condensed/")
-out_path = os.path.join(ROOT, "external/data/volumetric/condensed_masked/")
+def mask_image(image_file, out_file=None, dtype='int16'):
+    """
+    Read in and mask a single input image using rioxarray, with the option to
+    write masked image to disk. Mask is created from union of all bands' NA
+    values.
 
-# read in image files
-img_files = sorted(
-    glob.glob(os.path.join(data_path, '*.tif'))
-)
+        Params:
+            image_file (str): Path to local geotiff
+            out_file (str): Path/name for output geotiff (default is None)
+            dtype (str): Data type for masked image (default 'int16')
 
-# process files
-for i in img_files:
-    print("Processing " + os.path.basename(i))
-    stack = rxr.open_rasterio(i, masked=True).squeeze()
+        Returns:
+            A masked image in memory
+    """
+    image = rxr.open_rasterio(image_file, masked=True).squeeze()
 
     # create common mask (union of all NAs in each band)
-    mask = stack.mean(axis=0, skipna=False)
+    mask = image.mean(axis=0, skipna=False)
     mask = mask / mask
-    stack_mask = (stack * mask).astype('uint8') # save as 8 integer
+    masked_image = (image * mask).astype(dtype) # save as 8 integer
 
-    # get no data value from mask, write to temporary raster
-    nodata = stack_mask.min()
-    temp_stack = stack_mask.rio.write_nodata(nodata)  # set NA
+    # Write to new geotiff, if output path given
+    if out_file is not None:
+        nodata = masked_image.min()  # no data value
+        temp_image = masked_image.rio.write_nodata(nodata)  # set NA
+        temp_image.rio.to_raster(out_file)
 
-    # and then to new geotiff
-    out_name = re.sub(".tif", "_masked.tif", os.path.basename(i))
-    out_file = os.path.join(out_path, out_name)
-    temp_stack.rio.to_raster(out_file)
+    return masked_image
+
+def mask_images(image_files, dtype='int16'):
+    """
+    Read in and mask a list of input images and write to geotiff. The mask is
+    created from union of all bands' NA values. The file names of the masked
+    images are formed form the input names with '_masked' appended.
+
+        Params:
+            image_files (list): List of paths for local geotiffs
+            dtype (str): Data type for masked image (default 'int16')
+
+        Returns:
+            The last image in the list in memory.
+    """
+    for image in image_files:
+        image_name = os.path.basename(image)
+        masked_image_name = re.sub('.tif', '_masked.tif', image_name)
+        out_file = os.path.join(out_path, masked_image_name)
+
+        print('Processing {}'.format(image_name))
+
+        masked_image = mask_image(image, out_file, dtype)
+
+        return masked_image
